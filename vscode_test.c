285e95c6bb573edccfe073af42e63370a7576551
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-02-03 12:43:36 +0900

    플래그 배열 연습 ①

--- vscode_test.c
+++ vscode_test.c
@@ -4,106 +4,40 @@
 
 int main()
 {
-    // 플래그 변수 연습
-    /*
-    사용자가 문자열을 입력합니다. (예: apple7, password, 12345)
+    /*플래그배열 연습*/
 
-    이 문자열에 '숫자'가 하나라도 포함되어 있는지 확인하세요.
+    // 배열에 '5'가 있는지
+    printf("숫자 입력\n");
 
-    숫자가 있으면 "보안 등급 높음", 없으면 **"숫자를 포함해주세요"**라고 출력하세요.
-    */
-    printf("문자를 입력 : ");
-    char str[10] = "";
-    scanf("%s", str);
+    int arr[5] = {0};
+    int length = 5;
+    int flag[10] = {0}; // 플래그 배열 선언
 
-    // 플래그변수들
-    int hasDigit = 0;      // 숫자유무 확인
-    int hasCapital = 0;    // 대문자 확인
-    int hasLower = 0;      // 소문자 확인
-    int securityLevel = 0; // 보안등급
-
-    for (int i = 0; str[i] != '\0'; i++) // str문자가 null이 아닐때까지 반복
+    for (int i = 0; i < length; i++)
     {
-        if ('0' <= str[i] && str[i] <= '9') // 숫자나오는지 체크
-        {
-            hasDigit = 1; // 숫자가 나오면 플래그변수 1로
-        }
-        if ('A' <= str[i] && str[i] <= 'Z') // 대문자나오는지 체크
-        {
-            hasCapital = 1;
-        }
-        if ('a' <= str[i] && str[i] <= 'z')
-        {
-            hasLower = 1;
-        }
+        scanf("%d", &arr[i]); // arr배열에 숫자 입력받기
+    }
 
-        // 플래그변수들이 다 1이면 break
-        if (hasCapital == 1 && hasDigit == 1 && hasLower == 1)
+    // 찾기
+    for (int i = 0; i < length; i++)
+    {
+
+        /* if (arr[i] == 5) // 예시로 arr[3] = 5
         {
+            flag[i] = 1;
             break;
-        }
+        } */
+        int num = arr[i];
+        flag[num] = 1;
     }
 
-    // 보안 등급
-    if (hasDigit == 1)
+    if (flag[5] == 1)
     {
-        securityLevel++;
-    }
-    if (hasCapital == 1)
-    {
-        securityLevel++;
-    }
-    if (hasLower == 1)
-    {
-        securityLevel++;
-    }
-
-    // 숫자가 있을 때
-    if (hasDigit == 1)
-    {
-        printf("숫자포함O\n");
+        printf("있음\n");
     }
     else
     {
-        printf("숫자를 포함해주세요\n");
-    }
-
-    // 대문자 있을 때
-    if (hasCapital == 1)
-    {
-        printf("대문자포함O\n");
-    }
-    else
-    {
-        printf("대문자필요\n");
-    }
-
-    // 소문자 있을 때
-    if (hasLower == 1)
-    {
-        printf("소문자포함O\n");
-    }
-    else
-    {
-        printf("소문자필요\n");
-    }
-
-    // 보안등급 레벨 출력
-    if (securityLevel == 3)
-    {
-        printf("보안 등급: 매우 안전 🔒\n");
-    }
-    else if (securityLevel == 2)
-    {
-        printf("보안 등급: 안전 ✓\n");
-    }
-    else if (securityLevel == 1)
-    {
-        printf("보안 등급: 보통 ⚠️\n");
-    }
-    else
-    {
-        printf("보안 등급: 취약 ✗\n");
+        printf("없음\n");
     }
 
     return 0;
