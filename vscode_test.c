2c48a0179bb614ce69ed16260bd7f31d0de67be9
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-02-23 20:04:35 +0900

    백준 1152

--- vscode_test.c
+++ vscode_test.c
@@ -5,33 +5,36 @@
 int main()
 {
     /*
-    2675번
-몇번 반복할지 숫자 : 3
-abc 입력되면 : aaabbbccc
+   1152번
+   나온 문장에서 단어의 수 몇개인지
      */
 
-    printf("글자 입력 횟수 : ");
-    int total_num = 0;
-    scanf("%d", &total_num); // 여기에 2입력되면 2번만 입력받음
+    printf("글자 입력 : ");
+    char str[1000001] = ""; // 문자 받을 배열
+    scanf(" %[^\n]", str);  // 일반적인 %s로 하면, 띄어쓰기하면 그 뒤는 저장안됨
 
-    int string_num = 0; // 글자 몇번 반복하는지 받는 변수
-    char str[20] = "";  // 문자 입력받는 문자형 배열
+    // 배열 안의 단어 몇개인지 찾기
+    // hello world하면 2가 나와야함
+    int count = 0;
+    int in_word = 0;
 
-    for (int i = 0; i < total_num; i++) // 2번 반복
+    for (int i = 0; str[i] != '\0'; i++)
     {
-        scanf("%d %s", &string_num, str);
-        // printf("%c %c\n", str[0], str[0]); // 2번 넣으면 str[0]str[0]이렇게 두번 반복됨
-
-        // 문자열 반복
-        for (int j = 0; str[j] != '\0'; j++)
+        if (str[i] != ' ')
         {
-            // 몇번 반복 출력되는지
-            for (int k = 0; k < string_num; k++)
+            if (in_word == 0)
             {
-                printf("%c", str[j]);
+                count++;
+                in_word = 1;
             }
         }
-        printf("\n");
+        else
+        {
+            in_word = 0;
+        }
     }
+
+    printf("단어 수 : %d\n", count);
+
     return 0;
 }
