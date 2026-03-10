f5e3ad92220ce9db54431fb0b345e416f14ca54c
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-03-10 22:07:05 +0900

    백준 1316 (심화1)

--- vscode_test.c
+++ vscode_test.c
@@ -4,80 +4,46 @@
 
 int main()
 {
-    // 백준 2941
-    printf("문자입력:\n");
-    char str[101] = "";
-    scanf("%s", str);
+    // 백준 1316
+    // 그룹단어찾기
+    // aabb (O) abbx (X)
+    // 문자를 입력하자마자 그룹단어인지 체크하도록
 
-    int length = strlen(str);
-    for (int i = 0; i < length; i++)
+    printf("몇번 입력받을지 입력:\n");
+    int how_many_num = 0;
+    scanf("%d", &how_many_num);
+
+    int count = 0; // 그룹 단어 수 세기 용도
+
+    for (int i = 0; i < how_many_num; i++)
     {
-        if ('A' <= str[i] && str[i] <= 'Z')
+        char str[101] = "";
+        scanf("%s", str);
+        int length = strlen(str);
+
+        int alphabet[26] = {0};
+        int flag = 1; // 기본적으로 그룹단어라고 가정
+
+        for (int j = 0; j < length; j++) // 입력받은 문자열 반복하는 반복문
         {
-            str[i] = str[i] + 32;
+            // aabb 입력됐다고 가정
+            if (j > 0 && str[j] != str[j - 1])
+            {
+                if (alphabet[str[j] - 'a'] == 1)
+                {
+                    flag = 0; // 그룹 단어가 아니라고 명시
+                    break;
+                }
+            }
+            alphabet[str[j] - 'a'] = 1;
+        }
+        if (flag == 1)
+        {
+            count++;
         }
     }
 
-    // aaa
-    /*  for (int i = 0; i < 3; i++)
-     {
-         printf("0:%d\n", i);
-         if (str[i] == 'a')
-         {
-             i++;
-             printf("1:%d\n", i);
-         }
-         printf("2:%d\n", i);
-         count++;
-     } */
-
-    int count = 0;
-    for (int i = 0; i < length; i++)
-    {
-        // c- & c= 입력
-        if (str[i] == 'c')
-        {
-            if (str[i + 1] == '-' || str[i + 1] == '=')
-            {
-                i++;
-            }
-        }
-
-        // dz= & d- 입력
-        else if (str[i] == 'd')
-        {
-            if (str[i + 1] == 'z' && str[i + 2] == '=')
-            {
-                i += 2;
-            }
-            else if (str[i + 1] == '-')
-            {
-                i++;
-            }
-        }
-
-        // lj & nj
-        else if (str[i] == 'l' || str[i] == 'n')
-        {
-            if (str[i + 1] == 'j')
-            {
-                i++;
-            }
-        }
-
-        // s= & z=
-        else if (str[i] == 's' || str[i] == 'z')
-        {
-            if (str[i + 1] == '=')
-            {
-                i++;
-            }
-        }
-
-        count++;
-    }
-
-    printf("%d ", count);
+    printf("%d\n", count);
 
     return 0;
 }
