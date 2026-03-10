0eeb1a735f5ff30ffdd0a49a5d395f6398cd3ce6
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-03-10 19:57:22 +0900

    백준 2941 (심화1)

--- vscode_test.c
+++ vscode_test.c
@@ -1,42 +1,83 @@
 #include <stdio.h>
 #include <string.h> //strlen 사용시 필요
-#include <ctype.h>  //대문자<->소문자 변경 함수 사용시 필요  char upper = toupper(소문자변수);
+#include <ctype.h>  //소->대 char upper = toupper(소문자변수); //대->소 tolower(변수)
 
 int main()
 {
-    // 백준 1157
-    // 많이나온 알파벳 출력
-    // 많이 나온 알파벳이 겹치면 ? 출력
-
-    printf("단어 입력 : \n");
-    char str[1000001] = "";
+    // 백준 2941
+    printf("문자입력:\n");
+    char str[101] = "";
     scanf("%s", str);
 
-    // 소->대 변경, 알파벳배열에 글자 개수 넣기
-    int alphabet[26] = {0};
-
-    for (int i = 0; i < strlen(str); i++)
+    int length = strlen(str);
+    for (int i = 0; i < length; i++)
     {
-        alphabet[toupper(str[i]) - 'A']++;
-    }
-
-    // 알파벳 배열 안에서 최대값 찾기
-    int max = -1;
-    char letter;
-    for (int i = 0; i < 26; i++)
-    {
-        if (max < alphabet[i])
+        if ('A' <= str[i] && str[i] <= 'Z')
         {
-            max = alphabet[i];
-            letter = i + 'A'; // letter변수에 0+'A'해서 다시 문자로 만들기
-        }
-        else if (alphabet[i] == max && max != 0)
-        {
-            letter = '?';
+            str[i] = str[i] + 32;
         }
     }
 
-    printf("%c\n", letter);
+    // aaa
+    /*  for (int i = 0; i < 3; i++)
+     {
+         printf("0:%d\n", i);
+         if (str[i] == 'a')
+         {
+             i++;
+             printf("1:%d\n", i);
+         }
+         printf("2:%d\n", i);
+         count++;
+     } */
+
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        // c- & c= 입력
+        if (str[i] == 'c')
+        {
+            if (str[i + 1] == '-' || str[i + 1] == '=')
+            {
+                i++;
+            }
+        }
+
+        // dz= & d- 입력
+        else if (str[i] == 'd')
+        {
+            if (str[i + 1] == 'z' && str[i + 2] == '=')
+            {
+                i += 2;
+            }
+            else if (str[i + 1] == '-')
+            {
+                i++;
+            }
+        }
+
+        // lj & nj
+        else if (str[i] == 'l' || str[i] == 'n')
+        {
+            if (str[i + 1] == 'j')
+            {
+                i++;
+            }
+        }
+
+        // s= & z=
+        else if (str[i] == 's' || str[i] == 'z')
+        {
+            if (str[i + 1] == '=')
+            {
+                i++;
+            }
+        }
+
+        count++;
+    }
+
+    printf("%d ", count);
 
     return 0;
 }
