a072757cd39a8d5886a03a6afeff500c74dbe560
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-03-11 21:44:54 +0900

    백준 2941 (심화1 , 구조체&함수 사용)

--- vscode_test.c
+++ vscode_test.c
@@ -1,75 +1,109 @@
 #include <stdio.h>
 #include <string.h> //strlen 사용시 필요
 #include <ctype.h>  //소->대 char upper = toupper(소문자변수); //대->소 tolower(변수)
-struct Group_data
+// 구조체
+struct Letter
 {
     char str[101];
     int length;
 };
 
-int group_data_check(struct Group_data data)
+// 함수
+int Croatia_Alphabet(struct Letter let)
 {
-    int alphabet[26] = {0};
+    int count = 0;
 
-    for (int j = 0; j < data.length; j++) // 입력받은 문자열 반복하는 반복문
+    for (int i = 0; i < let.length; i++)
     {
-        // aabb 입력됐다고 가정
-        if (j > 0 && data.str[j] != data.str[j - 1])
+        if (let.str[i] == 'c')
         {
-            if (alphabet[data.str[j] - 'a'] == 1)
+            if (let.str[i + 1] == '-' || let.str[i + 1] == '=')
             {
-                return 0;
+                i++;
             }
         }
-        alphabet[data.str[j] - 'a'] = 1;
+        else if (let.str[i] == 'd')
+        {
+            if (let.str[i + 1] == '-')
+            {
+                i++;
+            }
+            else if (let.str[i + 1] == 'z' && let.str[i + 2] == '=')
+            {
+                i += 2;
+            }
+        }
+        else if (let.str[i] == 'l' || let.str[i] == 'n')
+        {
+            if (let.str[i + 1] == 'j')
+            {
+                i++;
+            }
+        }
+        else if (let.str[i] == 's' || let.str[i] == 'z')
+        {
+            if (let.str[i + 1] == '=')
+            {
+                i++;
+            }
+        }
+        count++;
     }
-    return 1;
+    return count;
 }
 
 int main()
 {
-    // 백준 1316
-    // 그룹단어찾기
-    // aabb (O) abbi (X)
-    // 문자를 입력하자마자 그룹단어인지 체크하도록
+    // 크로아티아 알파벳
+    // c는 -,= & d는 -, z= & j는 lj,nj & =는 s=,z=
 
-    printf("몇번 입력받을지 입력:\n");
-    int how_many_num = 0;
-    scanf("%d", &how_many_num);
+    struct Letter let;
+    printf("enter : ");
+    scanf("%s", let.str);
 
-    int count = 0; // 그룹 단어 수 세기 용도
+    let.length = strlen(let.str);
+    /* int count = 0;
 
-    for (int i = 0; i < how_many_num; i++)
+    for (int i = 0; i < length; i++)
     {
-        struct Group_data data_m;
-        scanf("%s", data_m.str);
-        data_m.length = strlen(data_m.str);
-
-        /* int alphabet[26] = {0};
-        int flag = 1; // 기본적으로 그룹단어라고 가정
-
-        for (int j = 0; j < length; j++) // 입력받은 문자열 반복하는 반복문
+        if (str[i] == 'c')
         {
-            // aabb 입력됐다고 가정
-            if (j > 0 && str[j] != str[j - 1])
+            if (str[i + 1] == '-' || str[i + 1] == '=')
             {
-                if (alphabet[str[j] - 'a'] == 1)
-                {
-                    flag = 0; // 그룹 단어가 아니라고 명시
-                    break;
-                }
+                i++;
             }
-            alphabet[str[j] - 'a'] = 1;
-        } */
-
-        int result = group_data_check(data_m);
-
-        if (result == 1)
-        {
-            count++;
         }
-    }
-    printf("%d\n", count);
+        else if (str[i] == 'd')
+        {
+            if (str[i + 1] == '-')
+            {
+                i++;
+            }
+            else if (str[i + 1] == 'z' && str[i + 2] == '=')
+            {
+                i += 2;
+            }
+        }
+        else if (str[i] == 'l' || str[i] == 'n')
+        {
+            if (str[i + 1] == 'j')
+            {
+                i++;
+            }
+        }
+        else if (str[i] == 's' || str[i] == 'z')
+        {
+            if (str[i + 1] == '=')
+            {
+                i++;
+            }
+        }
+        count++;
+    } */
+
+    int count = Croatia_Alphabet(let);
+
+    printf("%d ", count);
 
     return 0;
 }
