fd75c0dcd9b9a41249eb93f2597289e1f55c4b07
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-03-11 16:47:12 +0900

    백준 1316 (심화1 , 구조체&함수 사용)

--- vscode_test.c
+++ vscode_test.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 #include <string.h> //strlen 사용시 필요
 #include <ctype.h>  //소->대 char upper = toupper(소문자변수); //대->소 tolower(변수)
+struct Group_data
+{
+    char str[101];
+    int length;
+};
+
+int group_data_check(struct Group_data data)
+{
+    int alphabet[26] = {0};
+
+    for (int j = 0; j < data.length; j++) // 입력받은 문자열 반복하는 반복문
+    {
+        // aabb 입력됐다고 가정
+        if (j > 0 && data.str[j] != data.str[j - 1])
+        {
+            if (alphabet[data.str[j] - 'a'] == 1)
+            {
+                return 0;
+            }
+        }
+        alphabet[data.str[j] - 'a'] = 1;
+    }
+    return 1;
+}
 
 int main()
 {
     // 백준 1316
     // 그룹단어찾기
-    // aabb (O) abbx (X)
+    // aabb (O) abbi (X)
     // 문자를 입력하자마자 그룹단어인지 체크하도록
 
     printf("몇번 입력받을지 입력:\n");
@@ -17,11 +41,11 @@ int main()
 
     for (int i = 0; i < how_many_num; i++)
     {
-        char str[101] = "";
-        scanf("%s", str);
-        int length = strlen(str);
+        struct Group_data data_m;
+        scanf("%s", data_m.str);
+        data_m.length = strlen(data_m.str);
 
-        int alphabet[26] = {0};
+        /* int alphabet[26] = {0};
         int flag = 1; // 기본적으로 그룹단어라고 가정
 
         for (int j = 0; j < length; j++) // 입력받은 문자열 반복하는 반복문
@@ -36,13 +60,15 @@ int main()
                 }
             }
             alphabet[str[j] - 'a'] = 1;
-        }
-        if (flag == 1)
+        } */
+
+        int result = group_data_check(data_m);
+
+        if (result == 1)
         {
             count++;
         }
     }
-
     printf("%d\n", count);
 
     return 0;
