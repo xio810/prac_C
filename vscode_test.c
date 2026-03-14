7dbeb55f306872aa8452744ecfcd20c17437be54
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-03-14 17:50:24 +0900

    백준 2745 (수학1)

--- vscode_test.c
+++ vscode_test.c
@@ -1,81 +1,41 @@
 #include <stdio.h>
 #include <string.h> //strlen 사용시 필요
 #include <ctype.h>  //소->대 char upper = toupper(소문자변수); //대->소 tolower(변수)
-struct Subject
-{
-    char subject_name[51];
-    double subject_grade;
-    char score[3];
-};
-
-double calculate_gpa(struct Subject subjects[])
-{
-    double mul = 0;               // 과목학점 * 받은등급
-    double subject_grade_sum = 0; // 과목학점 전체 더하기
-
-    for (int i = 0; i < 20; i++)
-    {
-        double score_num = 0; // 등급에 따른 점수
-
-        scanf("%s %lf %s", subjects[i].subject_name, &subjects[i].subject_grade, subjects[i].score);
-
-        // 등급 별 점수
-        // 등급이 P인 과목은 계산에서 제외해야 한다.
-        if (subjects[i].score[0] == 'P')
-        {
-            continue;
-        }
-        // A+ & A0 은 4.5 & 4.0
-        if (subjects[i].score[0] == 'A')
-        {
-            score_num = (subjects[i].score[1] == '+') ? 4.5 : 4.0;
-        }
-
-        // B+ & B0 은 3.5 & 3.0
-        if (subjects[i].score[0] == 'B')
-        {
-
-            score_num = (subjects[i].score[1] == '+') ? 3.5 : 3.0;
-        }
-
-        // C+ & C0 은 2.5 & 2.0
-        if (subjects[i].score[0] == 'C')
-        {
-            score_num = (subjects[i].score[1] == '+') ? 2.5 : 2.0;
-        }
-
-        // D+ & D0 은 1.5 & 1.0
-        if (subjects[i].score[0] == 'D')
-        {
-            score_num = (subjects[i].score[1] == '+') ? 1.5 : 1.0;
-        }
-        // F는 0.0
-        if (subjects[i].score[0] == 'F')
-        {
-            score_num = 0.0;
-        }
-
-        mul += (subjects[i].subject_grade * score_num);
-        subject_grade_sum += subjects[i].subject_grade;
-    }
-    return mul / subject_grade_sum;
-}
 int main()
 {
-    // 구조체
-    struct Subject subject[20];
+    // 백준 2745
+    //  ZZZZZ 36 입력
 
-    printf("입력 : \n");
+    printf("입력:\n");
 
-    double result = calculate_gpa(subject);
+    char str[37] = ""; // ZZZ
+    int jin = 0;       // 진법
+    scanf("%s %d", str, &jin);
 
-    printf("%.6f\n", result);
+    int length = strlen(str);
 
-    /*
-    전공평점 계산 방법
-    ObjectOrientedProgramming1 3.0 A+(4.5)
-    IntroductiontoComputerEngineering 3.0 A+ 면
-    ( (3.0 * 4.5) + (3.0 * 4.5) ) / (3.0 + 3.0)
-    */
+    int result = 0;
+    int mul = 1;
+
+    for (int i = length - 1; i >= 0; i--)
+    {
+        int num = 0;
+
+        if ('A' <= str[i] && str[i] <= 'Z')
+        {
+            num = str[i] - 'A' + 10;
+        }
+        else
+        {
+            num = str[i] - '0';
+        }
+
+        result += mul * num;
+
+        mul = mul * jin;
+    }
+
+    printf("%d ", result);
+
     return 0;
 }
