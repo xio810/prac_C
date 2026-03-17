159910970240b2b576f5f619df02f5d9fd22b78d
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-03-17 20:49:08 +0900

    백준 2903 (수학1)

--- vscode_test.c
+++ vscode_test.c
@@ -4,44 +4,26 @@
 int main()
 {
 
-    printf("몇번 입력 받을 지 : ");
-    int test_case = 0;
-    scanf("%d", &test_case);
-
-    printf("금액 입력 : \n");
+    printf("숫자입력 : ");
+    //(2n제곱+1)제곱
+    // 2 -> 3 -> 5 -> 9 -> 17
+    // 4 -> 9 -> 25 ...
+    // 1번 입력 하면 9
     int num = 0;
-    int coins[4] = {25, 10, 5, 1};
-    int count[4] = {0};
+    scanf("%d", &num);
 
-    // 몫과 나머지
-    int mox = 0; // 몫
-    int nam = 0; // 나머지
+    int mul = 1; // 곱하는 값
+    int result = 0;
 
-    while (test_case--)
+    for (int i = 0; i < num; i++)
     {
-        scanf("%d", &num);
-
-        for (int i = 0; i < 4; i++)
-        {
-            mox = num / coins[i];
-            nam = num % coins[i];
-
-            if (num < coins[i])
-            {
-                count[i] = 0;
-            }
-            else
-            {
-
-                count[i] = mox;
-            }
-
-            printf("%d ", count[i]);
-
-            num = nam;
-        }
-        printf("\n");
+        mul = mul * 2;
     }
+    int ex = mul + 1;
+
+    result = ex * ex;
+
+    printf("%d\n", result);
 
     return 0;
 }
