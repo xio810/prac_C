3c88c32c2f185ccf57d8e8542c94db09b0ad5051
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-03-22 18:58:37 +0900

    백준 2501 (약수, 배수와 소수 1) (배열 사용)

--- vscode_test.c
+++ vscode_test.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 #include <string.h> //strlen 사용시 필요
 #include <ctype.h>  //소->대 char upper = toupper(소문자변수); //대->소 tolower(변수)
+
 int main()
 {
     printf("===>");
-    // 6 3 예시
+
     int divisor = 0; // 6의 약수
     int num = 0;     // 3번째로 작은 수 출력
     scanf("%d %d", &divisor, &num);
 
-    int count = 0;
+    int arr[10000] = {0}; // 약수담기
+
+    int j = 0;
 
     for (int i = 1; i <= divisor; i++)
     {
         if (divisor % i == 0)
         {
-            count++;
-            if (count == num)
-            {
-                printf("%d ", i);
-                break;
-            }
+            arr[j] = i;
+            j++;
         }
     }
 
-    if (count < num)
-    {
-        printf("0\n");
-    }
+    // 출력
+    printf("%d ", arr[num - 1]);
 
     return 0;
 }
