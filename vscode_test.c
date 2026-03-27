b24c5fd300c14311794ac18ed6e0c4489719dd51
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-03-27 13:04:41 +0900

    백준 3009 (기하 1: 직사각형과 삼각형 단계)

--- vscode_test.c
+++ vscode_test.c
@@ -6,29 +6,38 @@ int main()
 {
     printf("===>");
     // 입력 받을 수
-    int x, y, w, h = 0;
-    scanf("%d %d %d %d", &x, &y, &w, &h);
+    int x1, y1, x2, y2, x3, y3 = 0;
+    int x4, y4 = 0;
 
-    int right, left, top, bottom = 0;
+    scanf("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &x3, &y3);
 
-    left = x - 0;
-    right = w - x;
-    top = h - y;
-    bottom = y - 0;
-
-    int arr[4] = {left, right, top, bottom};
-
-    int min = arr[0];
-
-    for (int i = 0; i < 4; i++)
+    if (x1 == x2)
     {
-        if (min > arr[i])
-        {
-            min = arr[i];
-        }
+        x4 = x3;
+    }
+    else if (x2 == x3)
+    {
+        x4 = x1;
+    }
+    else
+    {
+        x4 = x2;
     }
 
-    printf("%d\n", min);
+    if (y1 == y3)
+    {
+        y4 = y2;
+    }
+    else if (y2 == y3)
+    {
+        y4 = y1;
+    }
+    else
+    {
+        y4 = y3;
+    }
+
+    printf("(%d %d)\n", x4, y4);
 
     return 0;
 }
